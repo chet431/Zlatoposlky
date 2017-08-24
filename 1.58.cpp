29867ec0fad137237a6cd8af30a31f667ed1a3d6
chet431 <shae777@mail.ru>
2017-08-25 01:24:42 +0300

    1.58 done

---
+++ 1.58.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	int far = 450;
+	int celcium = far * 1.8 + 32;
+	cout << "«начение в названии романа американского писател€-фантаста –э€ Ѕредбери 450 градусов по ‘аренгейту €вл€етс€ = " << celcium << " ÷ельси€м" << endl;
+	system("PAUSE");
+	return 0;
+}
